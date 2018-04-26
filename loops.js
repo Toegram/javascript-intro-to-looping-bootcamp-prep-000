e84467ce6e20122bafbde0a9d28c875d06edffd2
Anthony Turetsky <tturetsky@gmail.com>
2018-04-26 15:21:14 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,15 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push(`"I am ${i} strange loop."`)
+    } else if (i !== 1) {
+      array.push(`"I am ${i} strange loops."`)
+    } 
+  }
+  return array;
+}
+
+
+function whileLoop(array) {
+  while (var i )
+}
